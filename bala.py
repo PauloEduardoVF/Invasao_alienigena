e9d191d0b5e95ee2c619c868ec2937553bd752d2
PauloEduardoVF <pauloeduardo.vf@gmai.com>
2026-06-21 22:25:57 -0300

    Adicionando projétil e Criando a classe Bala

---
+++ bala.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bala(Sprite):
+    '''Classe para genrenciar os projéteis disparado da espaçonave'''
+    def __init__(self, ai_jogo):
+        super().__init__()
+        '''Cria um objeto Bala na posição atual da espaçonave'''
+        self.tela = ai_jogo.tela
+        self.configuracoes = ai_jogo.configuracoes
+        self.cor = self.configuracoes.bala_cor
+        
+        #cria uma bala em rect (0, 0) e, em seguida, define a posição correta
+        self.rect = pygame.Rect(0, 0, self.configuracoes.bala_largura,
+                                self.configuracoes.bala_altura)
+        self.rect.midtop = ai_jogo.espaconave.retangulo.midtop
+
+        #Armazena a posição do projétil como um float
+        self.y = float(self.rect.y)
+
+    def atualizar(self):
+        '''Desloca o projétil verticalmanete'''
+        #Atualiza a posição exata do projétil
+        self.y -= self.configuracoes.bala_velocidade
+        #Atualiza a posição do react
+        self.rect.y = self.y
+
+    def desenhar_bala(self):
+        '''Desenha o projétil na tela'''
+        pygame.draw.rect(self.tela, self.cor, self.rect)
--- configuracoes.py
+++ configuracoes.py
@@ -6,5 +6,11 @@ class Configuracoes:
         self.altura_tela = 800
         self.bg_cor = (230, 230, 230)
 
-         #Configuração da velovidade da nave.
+         #Configuração da velocidade da nave.
         self.nave_velocidade = 1.5
+
+        #Configurações do projétil
+        self.bala_velocidade = 2.0
+        self.bala_largura = 3
+        self.bala_altura = 15
+        self.bala_cor = (60, 60, 60)
